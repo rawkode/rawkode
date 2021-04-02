87454acf4da610d6bf19586e3f84071c91b603bb
Brian Ketelsen <bketelsen@gmail.com>
2021-04-02 14:11:16 -0400

    more updates

--- websites/api.brian.dev/api/index.js
+++ websites/api.brian.dev/api/index.js
@@ -3,7 +3,7 @@ const fetch = require("sync-fetch");
 import jsonGraphqlExpress from "json-graphql-server";
 
 const data = fetch(
-  "https://github.com/rawkode/rawkode/releases/download/blox/data.json"
+  "https://github.com/bketelsen/bkml/releases/download/blox/data.json"
 ).json();
 const app = require("express")();
 
