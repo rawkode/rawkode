17bb6e6ac56307d2f96e2ca86870dd6232d367ac
David McKay <david@rawkode.com>
2021-04-02 18:28:04 +0100

    fix: async fetch

--- websites/api.rawkode.dev/index.js
+++ websites/api.rawkode.dev/index.js
@@ -4,18 +4,20 @@ import fetch from "node-fetch";
 var data;
 const app = require("express")();
 
-fetch("https://github.com/rawkode/rawkode/releases/download/blox/data.json")
-  .then((res) => res.json())
-  .then((json) => {
-    data = json;
-    console.log(data);
-  })
-  .then(() => {
-    app.use("/", jsonGraphqlExpress(data));
+const run = () =>
+  fetch("https://github.com/rawkode/rawkode/releases/download/blox/data.json")
+    .then((res) => res.json())
+    .then((json) => {
+      data = json;
+      console.log(data);
+    })
+    .then(() => {
+      app.use("/", jsonGraphqlExpress(data));
+      const port = process.env.PORT || 3000;
+    });
 
-    const port = process.env.PORT || 3000;
-
-    module.exports = app.listen(port, () =>
-      console.log(`Server running on ${port}, http://localhost:${port}`)
-    );
-  });
+module.exports = run().then(() =>
+  app.listen(port, () =>
+    console.log(`Server running on ${port}, http://localhost:${port}`)
+  )
+);
