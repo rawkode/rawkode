627f85a40064b9b4ab0696d8ce64393bc3c23860
Brian Ketelsen <bketelsen@gmail.com>
2021-04-09 09:27:50 -0400

    try this with aswa

--- websites/azure.brian.dev/api/graphql/index.js
+++ websites/azure.brian.dev/api/graphql/index.js
@@ -1,16 +1,13 @@
 const fetch = require('sync-fetch');
 
 const jsonGraphqlExpress = require('json-graphql-server').default;
-
+const createHandler = require("azure-function-express").createHandler;
 
 const data = fetch('https://github.com/bketelsen/bkml/releases/download/blox/data.json').json();
 const app = require('express')();
 
 
-app.use('/', jsonGraphqlExpress(data));
+app.use('/api/graphql', jsonGraphqlExpress(data));
 console.log(data);
 
-// FUNCTIONS_HTTPWORKER_PORT still works, but guidance is to use FUNCTIONS_CUSTOMHANDLER_PORT
-const port = process.env.FUNCTIONS_CUSTOMHANDLER_PORT;
-
-module.exports = app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+module.exports = createHandler(app);
