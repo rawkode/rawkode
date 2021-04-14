75d1dd595ce0b5418ddb1348d43adc8aedd299b0
Brian Ketelsen <bketelsen@gmail.com>
2021-04-14 09:37:44 -0400

    update azure deployment

--- websites/azure.brian.dev/api/DataApi/index.js
+++ websites/azure.brian.dev/api/DataApi/index.js
@@ -3,10 +3,11 @@ const fetch = require('sync-fetch');
 const jsonGraphqlExpress = require('json-graphql-server').default;
 const createHandler = require("azure-function-express").createHandler;
 const jsonServer = require('json-server')
-const router = jsonServer.router(data, { foreignKeySuffix: '_id' })
 
 
 const data = fetch('https://github.com/bketelsen/bkml/releases/download/blox/data.json').json();
+
+const router = jsonServer.router(data, { foreignKeySuffix: '_id' })
 const app = require('express')();
 
 
