85bcd13ab8f6c904253e192d6b39278be9fd63c1
Brian Ketelsen <bketelsen@gmail.com>
2021-04-21 12:17:56 -0400

    after codemod

--- websites/brian.dev/components/mdx/callout.js
+++ websites/brian.dev/components/mdx/callout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-// https://www.notion.so/Callout-blocks-5b2638247b54447eb2e21145f97194b0
-export default ({
-  children,
-  background = 'bg-orange-100 dark:text-gray-800',
-  emoji = '💡'
-}) => {
+const Callout = (
+  {
+    children,
+    background = 'bg-orange-100 dark:text-gray-800',
+    emoji = '💡'
+  }
+) => {
   return (
     <div className={`${background} flex rounded-lg callout mt-6`}>
       <div className="pl-3 pr-2 py-2 select-none text-xl" style={{
@@ -16,4 +17,7 @@ export default ({
       <div className="pr-4 py-2">{children}</div>
     </div>
   )
-}
+};
+
+// https://www.notion.so/Callout-blocks-5b2638247b54447eb2e21145f97194b0
+export default Callout;
