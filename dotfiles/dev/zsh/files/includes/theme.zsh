a02a5f400b5b5d93ed4c1f07863861bf31931550
David McKay <david@rawkode.com>
2021-05-21 17:08:38 +0100

    chore: random changes I found on my disk

--- dotfiles/dev/zsh/files/includes/theme.zsh
+++ dotfiles/dev/zsh/files/includes/theme.zsh
@@ -1,7 +1,10 @@
-BASE16_THEME='chalk'
+BASE16_THEME='helios'
 
 zinit ice atload"base16_${BASE16_THEME}"
 zinit light "chriskempson/base16-shell"
 zinit ice lucid wait'0' \
     src"bash/base16-${BASE16_THEME}.config" \
     pick"bash/base16-${BASE16_THEME}.config" nocompile'!'
+
+
+# zinit light "dracula/zsh"
