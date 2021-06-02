799242e1a1038040762a934f4061f7b0d95db300
David McKay <david@rawkode.com>
2021-06-02 14:22:09 +0100

    feat: deploy k3s with Comtrya

---
+++ apps/awesome-o/src/index.ts
@@ -0,0 +1,20 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as metal from "@pulumi/equinix-metal";
+
+const config = new pulumi.Config();
+const projectId = config.require("projectID");
+
+const userData = `#!/usr/bin/env sh
+curl https://get.comtrya.dev | sh
+comtrya https://github.com/rawkode/rawkode#main:apps/awesome-o/opt/comtrya
+`;
+
+const k3sDevice = new metal.Device("k3s", {
+	hostname: "awesome-o",
+	billingCycle: metal.BillingCycle.Hourly,
+	operatingSystem: metal.OperatingSystem.Ubuntu2004,
+	plan: metal.Plan.C1LargeARM,
+	metro: "am",
+	projectId,
+	userData,
+});
