216c929ccc2cb3fb73ea9d012ef537907c72b284
David McKay <david@rawkode.com>
2021-12-29 18:12:10 +0000

    feat: deploy to vercel

--- websites/rawkode.dev/svelte.config.js
+++ websites/rawkode.dev/svelte.config.js
@@ -1,9 +1,8 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true,
@@ -11,8 +10,8 @@ const config = {
 	],
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: vercel(),
 	},
 };
 
