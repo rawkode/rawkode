005829b49ab869c5d52f07e3413b6d3a4042f296
David McKay <david@rawkode.com>
2021-12-29 18:34:12 +0000

    chore: update DNS for rawkode.dev

--- infrastructure/src/dns/rawkode.dev/index.ts
+++ infrastructure/src/dns/rawkode.dev/index.ts
@@ -19,7 +19,7 @@ zone.createRecord("google._domainkey", "TXT", [
   `"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjVevTfr7SjzrYEQ1YoI4yvBAnAfd2yqp6IQNZqZqm6HXJNXl2rBiy181NZU31Z9n6fGdoGT8yECZQBOL/9Ph9J9w/VO6ozOn0FkyEhYHkzq+twLHl5jWy07V0YeW4cg7CrV3oMSdv1kn5UgMSiB8zHoYQxlFsxM+MYg86OnY7ueT3jNbf+cVJUqFAV0R74I+n" "FoEerC92Gz7poMBRACRqs+wP9MHlzQnW+cjqMNci1fZwGRprTCTFPuWkDr75gnZDIPGa+cQAiVvtwZw6EK0KSytPAl5sDL7dHS12fGUPpaa05kJGezEhSn6wej37qFd+mn0NK0+H9ONsMtWAPQ72QIDAQAB"`,
 ]);
 
-zone.createRecord("@", "A", ["151.101.1.195", "151.101.65.195"]);
+zone.createRecord("@", "A", ["76.76.21.21"]);
 zone.createRecord("www", "CNAME", [`${DOMAIN}`]);
 
 export default zone;
