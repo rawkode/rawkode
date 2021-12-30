dd30af78c302a37c077e79cdc56e24d200afef4b
David McKay <david@rawkode.com>
2021-12-30 22:53:45 +0000

    fix: handle language aliases for Prism

--- websites/rawkode.dev/src/lib/portableText/code.svelte
+++ websites/rawkode.dev/src/lib/portableText/code.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
 	import Prism from 'prismjs';
+	import 'prismjs/components/prism-bash';
+	import 'prismjs/components/prism-css';
+	import 'prismjs/components/prism-docker';
+	import 'prismjs/components/prism-ini';
+	import 'prismjs/components/prism-markup';
+	import 'prismjs/components/prism-javascript';
+	import 'prismjs/components/prism-json';
+	import 'prismjs/components/prism-python';
+	import 'prismjs/components/prism-rust';
+	import 'prismjs/components/prism-typescript';
+	import 'prismjs/components/prism-yaml';
+
+	const extraLangMap = {
+		sh: 'bash',
+	};
+
+	const resolveLanguage = (lang: string) => {
+		return extraLangMap[lang] || lang;
+	};
+
 	import type { BlockProps } from '@portabletext/svelte';
 
 	export let portableText: BlockProps<{
@@ -21,8 +41,8 @@
 	<code>
       {@html Prism.highlight(
 			codeBlock.code,
-			Prism.languages[codeBlock.language],
-			codeBlock.language,
+			Prism.languages[resolveLanguage(codeBlock.language)],
+			resolveLanguage(codeBlock.language),
 		)}
 	</code>
 </pre>
