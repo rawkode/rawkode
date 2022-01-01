cc0aaf21155c30b014615ffc529bff75b76313ac
David McKay <david@rawkode.com>
2022-01-01 10:58:16 +0000

    feat: initial opengraph infrastructure

---
+++ websites/opengraph.rawkode.dev/src/index.ts
@@ -0,0 +1,15 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as gcpLegacy from "@pulumi/gcp";
+import * as gcp from "@pulumi/google-native";
+
+const enableCloudRun = new gcpLegacy.projects.Service("cloud-run", {
+  service: "run.googleapis.com",
+});
+
+const enableCloudFunctions = new gcpLegacy.projects.Service("cloud-functions", {
+  service: "cloudfunctions.googleapis.com",
+});
+
+const bucket = new gcp.storage.v1.Bucket("opengraph.rawkode.dev", {
+  name: "opengraph.rawkode.dev",
+});
