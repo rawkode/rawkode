83e6944cd171ccf9efad99ebb435184738b11950
David McKay <david@rawkode.com>
2022-01-02 11:21:26 +0000

    chore: bring build into Pulumi as Cloud Run requires GCR images

--- websites/opengraph.rawkode.dev/deploy/index.ts
+++ websites/opengraph.rawkode.dev/deploy/index.ts
@@ -1,17 +1,32 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as docker from '@pulumi/docker';
 import * as gcp from '@pulumi/google-native';
 
+const containerImage = new docker.Image('opengraph', {
+	imageName: pulumi.interpolate`gcr.io/${gcp.config.project}/opengraph-image-generator:latest`,
+	build: {
+		context: '../functions/image-generator'
+	}
+});
+
 const imageGenerator = new gcp.run.v1.Service('image-generator', {
+	kind: 'Service',
+	apiVersion: 'serving.knative.dev/v1',
+	metadata: {
+		name: 'opengraph-image-generator'
+	},
 	location: 'europe-west2',
 	spec: {
 		template: {
 			spec: {
 				containers: [
 					{
-						image: 'ghcr.io/rawkode/opengraph-image-generator:latest'
+						image: containerImage.imageName
 					}
 				]
 			}
 		},
+
 		traffic: [
 			{
 				latestRevision: true,
@@ -19,6 +34,17 @@ const imageGenerator = new gcp.run.v1.Service('image-generator', {
 			}
 		]
 	}
+});
+
+const imageGeneratorPolicy = new gcp.run.v1.ServiceIamPolicy('image-generator', {
+	serviceId: imageGenerator.id,
+	location: 'europe-west2',
+	bindings: [
+		{
+			members: ['allUsers'],
+			role: 'roles/run.invoker'
+		}
+	]
 });
 
 export const imageGeneratorUrl = imageGenerator.status.url;
