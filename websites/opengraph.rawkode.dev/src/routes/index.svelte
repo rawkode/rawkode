f4231797d682c227fd7e9f30f777cbb770b20859
David Flanagan <david@rawkode.com>
2022-01-04 17:25:02 +0000

    fix: add extra padding for Facebook and LinkedIn

--- websites/opengraph.rawkode.dev/src/routes/index.svelte
+++ websites/opengraph.rawkode.dev/src/routes/index.svelte
@@ -15,7 +15,7 @@
 		rounded"
 		>
 			<span
-				class="flex flex-row w-screen h-full content-center justify-start items-center bg-white/80"
+				class="flex flex-row w-screen h-full content-center justify-start items-center bg-white/80 pl-16 pr-16"
 			>
 				<img class="pl-8  p-4 h-full" src="/symbol.png" />
 				<p style="color: #292933;" class="flex-1 text-2xl pr-8 font-mono">https://rawkode.dev</p>
