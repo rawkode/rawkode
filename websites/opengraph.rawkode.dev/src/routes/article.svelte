84372089e4879fcc90cafea6ab1de0680a25ab50
David Flanagan <david@rawkode.com>
2022-01-04 18:35:32 +0000

    feat: add article graphic generation

--- websites/opengraph.rawkode.dev/src/routes/article.svelte
+++ websites/opengraph.rawkode.dev/src/routes/article.svelte
@@ -1,10 +1,37 @@
+<script context="module">
+	import { default as sanityClient } from '@sanity/client';
+
+	const client = sanityClient({
+		projectId: 'ypvmf3jx',
+		dataset: 'website',
+		apiVersion: '2021-03-25',
+		useCdn: true
+	});
+
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load({ url, params, stuff }) {
+		const query = `*[_type == "article" && _id == $id] {title, "slug": slug.current}`;
+
+		const articles = await client.fetch(query, { id: url.searchParams.get('id') });
+		if (articles.length === 0) {
+			return { status: 404 };
+		}
+
+		return { props: { title: articles[0].title } };
+	}
+</script>
+
+<script lang="ts">
+	export let title;
+</script>
+
 <main class="w-screen">
 	<div class="h-screen flex flex-col">
 		<h1
 			class="flex items-center content-center justify-center
-		rounded flex-grow text-6xl text-white "
+		rounded flex-grow text-8xl text-white "
 		>
-			Rawkode's Modern Life
+			{title}
 		</h1>
 		<div
 			class="flex h-32 items-center content-start justify-start
