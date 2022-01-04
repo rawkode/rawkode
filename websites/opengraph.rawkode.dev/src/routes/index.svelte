caf4b8ecf34fa8e05c92c9d4564eefbe3245540b
David Flanagan <david@rawkode.com>
2022-01-04 17:21:49 +0000

    feat: update opengraph default template with logo and links

--- websites/opengraph.rawkode.dev/src/routes/index.svelte
+++ websites/opengraph.rawkode.dev/src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { loop_guard } from 'svelte/internal';
+</script>
+
 <main class="w-screen">
 	<div class="h-screen flex flex-col">
 		<h1
@@ -10,7 +14,16 @@
 			class="flex h-32 items-center content-start justify-start
 		rounded"
 		>
-			<p class="p-16  bg-black/50  text-2xl text-white">//rawkode.dev</p>
+			<span
+				class="flex flex-row w-screen h-full content-center justify-start items-center bg-white/80"
+			>
+				<img class="pl-8  p-4 h-full" src="/symbol.png" />
+				<p style="color: #292933;" class="flex-1 text-2xl pr-8 font-mono">https://rawkode.dev</p>
+				<p class="flex-1 pr-8 font-mono text-2xl">
+					<span>Follow me on Twitter </span>
+					<span style="color: #00acee;"> @rawkode </span>
+				</p>
+			</span>
 		</div>
 	</div>
 </main>
