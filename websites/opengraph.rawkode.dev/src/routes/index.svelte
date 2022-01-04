a6511b35e64897e8b3a86f28f41fe50acd3b1a3a
David McKay <david@rawkode.com>
2022-01-04 10:04:35 +0000

    feat: default open graph image page

--- websites/opengraph.rawkode.dev/src/routes/index.svelte
+++ websites/opengraph.rawkode.dev/src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="w-screen">
+	<div class="h-screen flex flex-col">
+		<h1
+			class="flex items-center content-center justify-center
+		rounded flex-grow text-6xl text-white "
+		>
+			Rawkode's Modern Life
+		</h1>
+		<div
+			class="flex h-32 items-center content-start justify-start
+		rounded"
+		>
+			<p class="p-16  bg-black/50  text-2xl text-white">//rawkode.dev</p>
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		background-image: url('/background.png');
+	}
+</style>
--- websites/opengraph.rawkode.dev/svelte.config.js
+++ websites/opengraph.rawkode.dev/svelte.config.js
@@ -1,16 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess({})],
 
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
 };
