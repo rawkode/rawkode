c15372418678151da6e326b30c8660ae6083e8f4
David McKay <david@rawkode.com>
2022-01-05 11:29:44 +0000

    fix: handle CORS for tsdb

--- websites/tsdb.rawkode.dev/api/event.js
+++ websites/tsdb.rawkode.dev/api/event.js
@@ -10,7 +10,7 @@ import { InfluxDB, Point } from "@influxdata/influxdb-client";
 //   readonly p?: string;
 // };
 
-export default async function handler(_req, res) {
+export default async function handler(req, res) {
   const influxDB = new InfluxDB({
     url: process.env.INFLUXBD_URL,
     token: process.env.INFLUXDB_TOKEN,
@@ -39,3 +39,25 @@ export default async function handler(_req, res) {
   res.statusCode = 204;
   res.end();
 }
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*.rawkode.dev");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+module.exports = allowCors(handler);
