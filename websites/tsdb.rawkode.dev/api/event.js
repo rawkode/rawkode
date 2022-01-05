ebcd4849dfee26a9e273f5b33bacaee7ae06fd47
David Flanagan <david@rawkode.com>
2022-01-05 11:58:26 +0000

    fix: attempt to fix cors via vercel.json instead

--- websites/tsdb.rawkode.dev/api/event.js
+++ websites/tsdb.rawkode.dev/api/event.js
@@ -39,25 +39,3 @@ export default async function handler(req, res) {
   res.statusCode = 204;
   res.end();
 }
-
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "https://rawkode.dev");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-module.exports = allowCors(handler);
