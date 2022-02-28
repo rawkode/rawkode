49cbc22796b82ae61ec0aad8777659f4879853e4
David Flanagan <david@rawkode.com>
2022-02-28 16:39:51 +0000

    feat: add academy schema and some random dotfile changes

--- infrastructure/src/dns/rawkode.academy/index.ts
+++ infrastructure/src/dns/rawkode.academy/index.ts
@@ -4,7 +4,6 @@ const DOMAIN = "rawkode.academy";
 
 const zone = new Controller(DOMAIN);
 
-zone.createRecord("@", "A", ["18.184.197.212", "52.59.165.42"]);
 zone.createRecord("@", "TXT", [
   `"google-site-verification=dlh9jxVzubowYFoVO82naJOotuUwY8zNG2VYGWlDhsU"`,
   `"v=spf1 include:_spf.google.com ~all"`,
@@ -15,5 +14,8 @@ zone.createMxRecord("@", 5, "alt1.aspmx.l.google.com");
 zone.createMxRecord("@", 5, "alt2.aspmx.l.google.com");
 zone.createMxRecord("@", 10, "alt3.aspmx.l.google.com");
 zone.createMxRecord("@", 10, "alt4.aspmx.l.google.com");
+
+zone.createRecord("@", "A", ["76.76.21.21"]);
+zone.createRecord("schema", "CNAME", [`${DOMAIN}`]);
 
 export default zone;
