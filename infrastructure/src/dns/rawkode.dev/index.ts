85376a8ed85e9936613cc3a0564ac75ab12e4c2c
David Flanagan <david@rawkode.dev>
2022-06-28 10:03:47 +0100

    feat: switch personal email to FastMail

--- infrastructure/src/dns/rawkode.dev/index.ts
+++ infrastructure/src/dns/rawkode.dev/index.ts
@@ -4,14 +4,21 @@ const DOMAIN = "rawkode.dev";
 
 const zone = new Controller(DOMAIN);
 
-zone.createMxRecord("@", 1, "aspmx.l.google.com");
-zone.createMxRecord("@", 5, "alt1.aspmx.l.google.com");
-zone.createMxRecord("@", 5, "alt2.aspmx.l.google.com");
-zone.createMxRecord("@", 10, "alt3.aspmx.l.google.com");
-zone.createMxRecord("@", 10, "alt4.aspmx.l.google.com");
+zone.createMxRecord("@", 10, "in1-smtp.messagingengine.com");
+zone.createMxRecord("@", 20, "in2-smtp.messagingengine.com");
+
+zone.createRecord("fm1._domainkey", "CNAME", [
+  "fm1.rawkode.dev.dkim.fmhosted.com",
+]);
+zone.createRecord("fm2._domainkey", "CNAME", [
+  "fm2.rawkode.dev.dkim.fmhosted.com",
+]);
+zone.createRecord("fm3._domainkey", "CNAME", [
+  "fm3.rawkode.dev.dkim.fmhosted.com",
+]);
 
 zone.createRecord("@", "TXT", [
-  `"v=spf1 include:_spf.google.com ~all"`,
+  `"v=spf1 include:spf.messagingengine.com ?all"`,
   `"google-site-verification=RnbvxDOTdobTiAetVoa-U3Xc0Irk76nan_OcRCGuQTM"`,
 ]);
 
