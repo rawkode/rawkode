7a87b8b77a356f4562230249b5a19bf6a39ca09f
David Flanagan <david@rawkode.dev>
2023-03-16 11:12:27 +0000

    chore: rawkode.dev website and bun stuff

--- projects/core-infrastructure/dns/src/domains/rawkode.dev.ts
+++ projects/core-infrastructure/dns/src/domains/rawkode.dev.ts
@@ -4,7 +4,7 @@ import { ManagedDomain } from "../dnsProvider";
 export default (scope: Construct): ManagedDomain => {
 	const managedDomain = new ManagedDomain(scope, "rawkode.dev");
 
-	managedDomain.enableFastmail();
+	managedDomain.enableFastmail().addARecord("@", "3.13.222.255");
 
 	return managedDomain;
 };
