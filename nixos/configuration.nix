dde8b98f8be9115ab5834ee32fc242f8490cc074
David Flanagan <david@rawkode.dev>
2024-05-31 17:24:31 +0100

    fix: use gnome polkit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -27,6 +27,22 @@
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
   programs.niri.enable = true;
 
+  systemd = {
+    user.services.niri-flake-polkit.enable = false;
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
   nixpkgs.config = {
     allowUnfree = true;
   };
