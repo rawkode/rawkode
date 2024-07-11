757e0b69d7ed85691ede7831becb4b940e4c6cb4
David Flanagan <david@rawkode.dev>
2024-07-11 17:15:59 +0100

    feat(rawkOS): 1password and vivaldi

--- rawkOS/build.sh
+++ rawkOS/build.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env sh
 set -ouex pipefail
 
-cat << EOF | sudo tee -a /etc/yum.repos.d/1password.repo
-[1password]
-name=1Password Stable Channel
-baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://downloads.1password.com/linux/keys/1password.asc
-EOF
-
-rpm-ostree install 1password 1password-cli
