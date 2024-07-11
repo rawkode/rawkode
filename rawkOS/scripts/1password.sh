6c6352efe9c67a6f355fdb4db7b39d5160b87a56
David Flanagan <david@rawkode.dev>
2024-07-11 23:20:52 +0100

    fix(rawkOS): 1password with vivaldi

--- rawkOS/scripts/1password.sh
+++ rawkOS/scripts/1password.sh
@@ -38,3 +38,8 @@ chmod g+s /usr/bin/op
 cat >/usr/lib/sysusers.d/onepassword.conf <<EOF
 g     onepassword-cli /usr/bin/op
 EOF
+
+mkdir -p /etc/1password
+cat >/etc/1password/custom_allowed_browsers <<EOF
+vivaldi
+EOF
