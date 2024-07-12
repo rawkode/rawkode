78ce3d9c0ead6e391ba97639e32b570879370106
David Flanagan <david@rawkode.dev>
2024-07-12 09:28:57 +0100

    fix(rawkOS): absolute path for sandbox

--- rawkOS/scripts/1password.sh
+++ rawkOS/scripts/1password.sh
@@ -21,19 +21,19 @@ rm /usr/bin/1password
 mv /var/opt/1Password /usr/lib/1Password
 ln -s /opt/1Password/1password /usr/bin/1password
 
-chmod 4755 ./chrome-sandbox
+chmod 4755 /usr/lib/1Password/chrome-sandbox
 
 GID_ONEPASSWORD="1500"
 GID_ONEPASSWORDCLI="1600"
+
+cat >/usr/lib/tmpfiles.d/onepassword.conf <<EOF
+L  /opt/1Password  -  -  -  -  /usr/lib/1Password
+EOF
 
 BROWSER_SUPPORT_PATH="/usr/lib/1Password/1Password-BrowserSupport"
 
 chgrp "${GID_ONEPASSWORD}" "${BROWSER_SUPPORT_PATH}"
 chmod g+s "${BROWSER_SUPPORT_PATH}"
-
-cat >/usr/lib/tmpfiles.d/onepassword.conf <<EOF
-L  /opt/1Password  -  -  -  -  /usr/lib/1Password
-EOF
 
 chown root:${GID_ONEPASSWORDCLI} /usr/bin/op
 chmod g+s /usr/bin/op
