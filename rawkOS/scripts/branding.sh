59fe578ed9557c9ae5a8a73e3ab9de60bb0822bc
David Flanagan <david@rawkode.dev>
2024-07-15 12:13:56 +0100

    feat(rawkOS): rebase on kinoite

--- rawkOS/scripts/branding.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-set -ouex pipefail
-
-sed -i '/^PRETTY_NAME/s/Silverblue/rawkOS/' /usr/lib/os-release
-sed -i 's/Bluefin/rawkOS/g' /usr/etc/yafti.yml
-sed -i 's/Aurora (Beta)/rawkOS/' /usr/etc/yafti.yml
-sed -i 's/Bluefin/rawkOS/' /usr/libexec/ublue-flatpak-manager
