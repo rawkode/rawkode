749a49ddadd963f1c5dbd2319f91b06f932c51c8
David Flanagan <david@rawkode.dev>
2024-08-22 10:02:43 +0100

    fix(nix): tidy hyprlock

--- nix/home-manager/desktop/hyprland/hyprlock.nix
+++ nix/home-manager/desktop/hyprland/hyprlock.nix
@@ -1,52 +1,59 @@
+{ lib, pkgs, ... }:
 {
-  lib,
-  pkgs,
-  ...
-}:
-{
-  # Hyprlock is a lockscreen that is a part of the hyprland suite
-  # This config provides a lockscreen with a background image, input field, and labels
   programs = {
     hyprlock = {
       enable = true;
       settings = {
         general = {
-          grace = 5;
+          grace = 300;
+          disable_loading_bar = true;
           hide_cursor = true;
+          no_fade_in = false;
         };
-        background = [{
-          path = "/etc/backgrounds/DeterminateColorway-1920x1080.png";
-        }];
-        input-field = [{
-          size = "480, 108";
-          outer_color = "rgba(147, 153, 178, 1.0)";
-          #inner_color = "rgba(88, 91, 112. 1.0)";
-          font_color = "rgba(127, 132, 156, 1.0)";
-          placeholder_text = "enter password";
-        }];
-        label = [{
-          text = "🔐";
-          color = "rgba(249, 226, 175, 1.0)";
-          font_family = "Work Sans";
-          font_size = 192;
-          text_align = "center";
-          halign = "center";
-          valign = "center";
-          position = "0, 300";
-        }
-        {
-          text = "$TIME";
-          color = "rgba(205, 214, 244, 1.0)";
-          font_family = "Work Sans";
-          font_size = 96;
-          text_align = "center";
-          halign = "center";
-          valign = "center";
-          position = "0, -250";
-        }];
+
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 2;
+            blur_size = 4;
+          }
+        ];
+
+        input-field = [
+          {
+            size = "720, 108";
+            outer_color = "rgba(147, 153, 178, 1.0)";
+            font_color = "rgba(127, 132, 156, 1.0)";
+            placeholder_text = " ... ";
+          }
+        ];
+
+        label = [
+          {
+            text = "🏴󠁧󠁢󠁳󠁣󠁴󠁿";
+            color = "rgba(249, 226, 175, 1.0)";
+            font_family = "Monaspace Krypton";
+            font_size = 192;
+            text_align = "center";
+            halign = "center";
+            valign = "center";
+            position = "0, 300";
+          }
+          {
+            text = "$TIME";
+            color = "rgba(205, 214, 244, 1.0)";
+            font_family = "Monaspace Krypton";
+            font_size = 96;
+            text_align = "center";
+            halign = "center";
+            valign = "center";
+            position = "0, -250";
+          }
+        ];
       };
     };
   };
+
   services = {
     hypridle = {
       enable = true;
@@ -69,11 +76,10 @@
       };
     };
   };
+
   wayland.windowManager.hyprland = {
     settings = {
-      bind = [
-        "$mainMod, L, exec, ${lib.getExe pkgs.hyprlock} --immediate"
-        ];
+      bind = [ "$mainMod, L, exec, ${lib.getExe pkgs.hyprlock} --immediate" ];
     };
   };
 }
