8b975499598522598434ebc395c85db22483a98d
David Flanagan <david@rawkode.dev>
2024-08-28 11:37:42 +0100

    chore(nix): switch to podman

--- nix/modules/nixos/containers/default.nix
+++ nix/modules/nixos/containers/default.nix
@@ -1,8 +1,21 @@
+{ pkgs, ... }:
 {
   virtualisation = {
     containers.enable = true;
+
     lxc.enable = true;
     lxd.enable = true;
-    docker.enable = true;
+
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
   };
+
+  environment.systemPackages = with pkgs; [
+    dive
+    docker-compose
+    podman-tui
+  ];
 }
