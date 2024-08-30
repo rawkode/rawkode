f7acd63288bb922095e2905ab5b0177f50793349
David Flanagan <david@rawkode.dev>
2024-08-30 19:52:02 +0100

    chore(nix): ssdm

--- nix/modules/nixos/desktop/kde/default.nix
+++ nix/modules/nixos/desktop/kde/default.nix
@@ -19,7 +19,6 @@ in
 
   config = mkIf cfg.enable {
     services.xserver.enable = true;
-    services.xserver.displayManager.gdm.enable = true;
 
     services.desktopManager.plasma6.enable = true;
     services.displayManager.defaultSession = "plasma";
@@ -29,8 +28,9 @@ in
       configPackages = with pkgs; [ xdg-desktop-portal-kde ];
     };
 
-    services.displayManager.sddm.enable = false;
-    services.displayManager.sddm.wayland.enable = false;
+    services.displayManager.sddm.enable = true;
+    services.displayManager.sddm.wayland.enable = true;
+    services.xserver.displayManager.gdm.enable = mkForce false;
 
     programs.ssh.askPassword = mkForce "${pkgs.ksshaskpass}/bin/ksshaskpass";
 
