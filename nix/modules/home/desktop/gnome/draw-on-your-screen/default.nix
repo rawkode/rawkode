a2c4cfea4c2e18f0ba0049de67b8e796bd0f8550
David Flanagan <david@rawkode.dev>
2024-09-11 13:51:55 +0100

    chore(nix): draw screen config

--- nix/modules/home/desktop/gnome/draw-on-your-screen/default.nix
+++ nix/modules/home/desktop/gnome/draw-on-your-screen/default.nix
@@ -17,7 +17,7 @@ in
         enabled-extensions = [ "draw-on-your-screen2@zhrexl.github.com" ];
       };
 
-      "org/gnome/shell/extensions/draw-on-your-screen2" = {
+      "org/gnome/shell/extensions/draw-on-your-screen" = {
         erase-drawings = [ "<Super><Shift>d" ];
         toggle-drawing = [ "<Super>d" ];
         toggle-modal = [ "<Control><Alt><Super>d" ];
