1eb6a70782ce432083c542d03efbb269b48aa7e5
David Flanagan <david@rawkode.dev>
2024-09-25 12:50:46 +0100

    chore(rawkOS): add cloudflare warp

---
+++ rawkOS/cloudflare-warp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eoux pipefail
+
+cat <<EOF | sudo tee /etc/yum.repos.d/cloudflare-warp.repo
+[cloudflare-warp-stable]
+name=cloudflare-warp-stable
+baseurl=https://pkg.cloudflareclient.com/rpm
+enabled=1
+type=rpm
+gpgcheck=1
+gpgkey=https://pkg.cloudflareclient.com/pubkey.gpg
+EOF
+
+rpm --import https://pkg.cloudflareclient.com/pubkey.gpg
+
+rpm-ostree install cloudflare-warp
+
+rm -f /etc/yum.repos.d/cloudflare-warp.repo
