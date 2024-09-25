9d3dcbb02fb41d4f76951cc588fb842ed5fc1889
David Flanagan <david@rawkode.dev>
2024-09-25 13:26:19 +0100

    fix(rawkOS): escape dollars

--- rawkOS/firefox.sh
+++ rawkOS/firefox.sh
@@ -4,7 +4,7 @@ set -eoux pipefail
 cat <<EOF | sudo tee /etc/yum.repos.d/firefox-dev.repo
 [copr:copr.fedorainfracloud.org:the4runner:firefox-dev]
 name=Copr repo for firefox-dev owned by the4runner
-baseurl=https://download.copr.fedorainfracloud.org/results/the4runner/firefox-dev/fedora-$releasever-$basearch/
+baseurl=https://download.copr.fedorainfracloud.org/results/the4runner/firefox-dev/fedora-\$releasever-\$basearch/
 type=rpm-md
 skip_if_unavailable=True
 gpgcheck=1
