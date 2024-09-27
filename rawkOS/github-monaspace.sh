64a225241f89f1120663af6a51c6337a388c3375
David Flanagan <david@rawkode.dev>
2024-09-27 22:04:37 +0100

    fix(rawkOS): font installer

--- rawkOS/github-monaspace.sh
+++ rawkOS/github-monaspace.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -eoux pipefail
 
+cd /tmp
+
 git clone https://github.com/githubnext/monaspace
+
 cd monaspace
-bash ./util/install_linux.sh
+
+mkdir -p /usr/share/fonts/Monaspace/
+
+cp ./fonts/otf/* /usr/share/fonts/Monaspace/
+cp ./fonts/variable/* /usr/share/fonts/Monaspace/
+
+fc-cache --system-only --really-force /usr/share/fonts/Monaspace/
+
+rm -rf /tmp/monaspace
