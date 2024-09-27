0a9170563b9f065055fdd562ad4f867f8b8e8ebf
David Flanagan <david@rawkode.dev>
2024-09-27 18:31:28 +0100

    chore(rawkOS): use fish for interactive sessions

--- rawkOS/fish.sh
+++ rawkOS/fish.sh
@@ -3,15 +3,14 @@ set -eoux pipefail
 
 rpm-ostree install fish
 
-# We add this, because on fresh installs, my shell will be bash
-# and it's likely I'll install Nix with that configured.
-# In this case, the installer prefers bash with /etc/profile.d
-# but not fish.
-# This means when I eventually change my shell, we lose the XDG_DATA_DIRS
-# config that makes my GNOME extensions work
-mkdir -p /etc/fish/conf.d
-cat <<EOF | sudo tee /etc/fish/conf.d/nix.sh
-if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-    . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-end
+# Changing my default shell to fish means that GNOME
+# doesn't get updated with the paths I need for nix
+# So let's set up bash to invoke fish for interactive
+# sessions.
+cat <<EOF > /etc/profile.d/fish.sh
+if [[ $(ps --no-header --pid=\$PPID --format=comm) != "fish" && -z \${BASH_EXECUTION_STRING} && \${SHLVL} == 1 ]]
+then
+	shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
+	exec fish \$LOGIN_OPTION
+fi
 EOF
