99161028ac225ef109739c892557ca9595d85201
David Flanagan <david@rawkode.dev>
2024-09-30 11:52:19 +0100

    feat(rawkOS): arkane but with CachyOS

---
+++ arch/arkdep-build.d/rawkOS/update.sh
@@ -0,0 +1,4 @@
+if [[ -f $arkdep_boot/loader/entries/${data[0]}.conf ]]; then
+	mv $arkdep_boot/loader/entries/${data[0]}.conf $arkdep_boot/loader/entries/$(date +%Y%m%d-%H%M%S)-${data[0]}+3.conf
+	bootctl set-default ''
+fi
