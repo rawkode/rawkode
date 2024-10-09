45b22b75f36a60775a6ba7378fa178d07ac0733c
David Flanagan <david@rawkode.dev>
2024-10-09 21:57:17 +0100

    fix(rawkOS): color scheme for wezterm

--- nix/modules/home/desktop/wezterm/config/wezterm.lua
+++ nix/modules/home/desktop/wezterm/config/wezterm.lua
@@ -7,9 +7,9 @@ if wezterm.config_builder then
     -- There's no decent way to have Zellij or fish
 		-- update yet; so pending
     if appearance.is_dark() then
-      config.color_scheme = 'catppuccin-latte'
-    else
       config.color_scheme = 'catppuccin-frappe'
+    else
+      config.color_scheme = 'catppuccin-latte'
     end
 
     config.automatically_reload_config = true
