d412703f0fcdbf824e22d93bb52079d5798db703
David Flanagan <david@rawkode.dev>
2024-11-11 17:05:20 +0000

    fix(rawkOS): profile.d directory for auto-fish

--- rawkOS/command-line/fish/mod.ts
+++ rawkOS/command-line/fish/mod.ts
@@ -4,7 +4,7 @@ export default async () => {
 	await installPackages(["fish"]);
 
 	Deno.writeFileSync(
-		"/etc/profile/0-auto-fish.sh",
+		"/etc/profile.d/0-auto-fish.sh",
 		Deno.readFileSync(`${import.meta.dirname}/auto-fish.sh`),
 	);
 };
