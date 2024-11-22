7d97a503d60437692278668c9ed24456654aca9f
David Flanagan <david@rawkode.dev>
2024-11-22 16:43:23 +0000

    fix: wayland things

--- rawkOS/desktop/niri/mod.ts
+++ rawkOS/desktop/niri/mod.ts
@@ -1,6 +1,8 @@
 import { ensureHomeSymlink } from "../../utils/files/mod.ts";
 import { archInstall } from "../../utils/package/mod.ts";
 
-archInstall(["niri", "walker-bin"]);
+archInstall(["niri", "mako", "walker-bin"]);
 
 ensureHomeSymlink(import.meta.dirname + "/config.kdl", ".config/niri/config.kdl");
+ensureHomeSymlink(import.meta.dirname + "/walker.json", ".config/walker/config.json");
+
--- rawkOS/desktop/visual-studio-code/mod.ts
+++ rawkOS/desktop/visual-studio-code/mod.ts
@@ -1,5 +1,8 @@
+import { ensureHomeSymlink } from "../../utils/files/mod.ts";
 import { archInstall } from "../../utils/package/mod.ts";
 
 await archInstall([
   "visual-studio-code-bin",
 ]);
+
+ensureHomeSymlink(import.meta.dirname + "/argv.json", ".vscode/argv.json");
