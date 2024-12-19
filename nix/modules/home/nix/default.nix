5bdae15d3934bd3f550706d1e85b62e41ac337b1
David Flanagan <david@rawkode.dev>
2024-12-19 11:44:13 +0000

    feat: nix helper for git revision hash

--- nix/modules/home/nix/default.nix
+++ nix/modules/home/nix/default.nix
@@ -1,5 +1,15 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    nixd
-  ];
+{ ... }:
+{
+  programs.fish.functions = {
+    nixpkgs-hash-git = {
+      description = "Get a nixpkgs hash for a Git revision";
+      argumentNames = [
+        "repoUrl"
+        "revision"
+      ];
+      body = ''
+        nix-shell -p nix-prefetch-git jq --run "nix hash convert sha256:\$(nix-prefetch-git --url $repoUrl --quiet --rev $revision | jq -r '.sha256')"
+      '';
+    };
+  };
 }
