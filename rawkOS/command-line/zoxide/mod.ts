c8705c98e9bb162fb82230530557058aa5ffe9db
David Flanagan <david@rawkode.academy>
2025-05-18 00:06:21 +0100

    fix(rawkOS): zoxide is harded coded to skim

--- rawkOS/command-line/zoxide/mod.ts
+++ rawkOS/command-line/zoxide/mod.ts
@@ -2,7 +2,7 @@ import { $ } from "bun";
 import { ensureHomeSymlink } from "../../utils/files/mod.ts";
 import { archInstall } from "../../utils/package/mod.ts";
 
-await archInstall(["zoxide", "skim"]);
+await archInstall(["zoxide", "fzf"]);
 
 await $`nu -c 'zoxide init nushell | save -f ($nu.user-autoload-dirs | path join "zoxide.nu")'`;
 
