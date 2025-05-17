2b51ed641cca71c009034633521aafc061f538bb
David Flanagan <david@rawkode.dev>
2025-05-17 22:02:48 +0100

    feat(rawkOS): bringing back niri

---
+++ rawkOS/desktop/niri/mod.ts
@@ -0,0 +1,19 @@
+import { ensureHomeSymlink } from "../../utils/files/mod.ts";
+import { archInstall } from "../../utils/package/mod.ts";
+
+await archInstall([
+  "niri",
+  "catppuccin-cursors-mocha",
+  "gauntlet-bin",
+  "mako",
+  "waybar",
+  "xwayland-satellite",
+]);
+
+ensureHomeSymlink(
+  import.meta.dirname + "/config.kdl",
+  ".config/niri/config.kdl",
+);
+
+await import("./waybar/mod.ts");
+await import("./waybar/mod.ts");
