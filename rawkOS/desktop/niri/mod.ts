487ec3866d6d9a1a48f50178622dad9f13b2430a
David Flanagan <david@rawkode.academy>
2025-05-19 10:43:16 +0100

    fix(rawkOS): ensure 1password runs on x11
    We get no notification prompts for op CLI if we run on wayland

--- rawkOS/desktop/niri/mod.ts
+++ rawkOS/desktop/niri/mod.ts
@@ -2,23 +2,19 @@ import { ensureHomeSymlink } from "../../utils/files/mod.ts";
 import { archInstall } from "../../utils/package/mod.ts";
 
 await archInstall([
-  "niri",
   "catppuccin-cursors-mocha",
   "gauntlet-bin",
   "mako",
+  "niri",
   "polkit-gnome",
+  "seahorse",
   "xwayland-satellite",
 ]);
+
+await import("./waybar/mod.ts");
+await import("./swaync/mod.ts");
 
 ensureHomeSymlink(
   `${import.meta.dirname}/config.kdl`,
   ".config/niri/config.kdl",
 );
-
-ensureHomeSymlink(
-  `${import.meta.dirname}/portals.conf`,
-  ".config/xdg-desktop-portal/niri-portals.conf",
-);
-
-await import("./waybar/mod.ts");
-await import("./swaync/mod.ts");
