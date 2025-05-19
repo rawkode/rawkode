1c273122d19ce5578967b278cbe9f55a54a424ff
David Flanagan <david@rawkode.academy>
2025-05-19 10:52:28 +0100

    chore(rawkOS): tweak ghostty config

--- rawkOS/desktop/ghostty/mod.ts
+++ rawkOS/desktop/ghostty/mod.ts
@@ -2,7 +2,8 @@ import { ensureHomeSymlink } from "../../utils/files/mod.ts";
 import { archInstall } from "../../utils/package/mod.ts";
 
 await archInstall(["ghostty"]);
+
 ensureHomeSymlink(
-	`${import.meta.dirname}/config.ini`,
-	".config/ghostty/config",
+  `${import.meta.dirname}/config.ini`,
+  ".config/ghostty/config",
 );
