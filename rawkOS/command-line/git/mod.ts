ce57d9ee56d6bcc0f313844dd4f124d7cca05c0d
David Flanagan <david@rawkode.academy>
2025-05-21 10:55:01 +0100

    chore(rawkOS): enable gitsign

--- rawkOS/command-line/git/mod.ts
+++ rawkOS/command-line/git/mod.ts
@@ -12,31 +12,33 @@ const gitSignCredentialCachePath = `${home}/Code/bin`;
 await archInstall(["gitsign"]);
 
 const credentialCache = await fetch(
-	"https://github.com/sigstore/gitsign/releases/download/v0.11.0/gitsign-credential-cache_0.11.0_linux_amd64",
+  "https://github.com/sigstore/gitsign/releases/download/v0.11.0/gitsign-credential-cache_0.11.0_linux_amd64",
 );
 
 mkdirSync(gitSignCredentialCachePath, { recursive: true });
 
 writeFileSync(
-	`${gitSignCredentialCachePath}/gitsign-credential-cache`,
-	new Uint8Array(await credentialCache.arrayBuffer()),
+  `${gitSignCredentialCachePath}/gitsign-credential-cache`,
+  new Uint8Array(await credentialCache.arrayBuffer()),
 );
 
 chmodSync(`${gitSignCredentialCachePath}/gitsign-credential-cache`, 0o755);
 
-new SystemdUnit("gitsign-credential-cache.service", {
-	description: "GitSign credential cache",
-	type: "simple",
-	scope: "user",
-	execStart: `${gitSignCredentialCachePath}/gitsign-credential-cache`,
-	wantedBy: "default.target",
+new SystemdUnit("gitsign-credential-cache.socket", {
+  type: "socket",
+  scope: "user",
+  description: "GitSign credential cache socket",
+  listenStream: "%C/sigstore/gitsign/cache.sock",
+  directoryMode: "0700",
+  wantedBy: "default.target",
 }).install();
 
-new SystemdUnit("gitsign-credential-cache.socket", {
-	type: "socket",
-	scope: "user",
-	description: "GitSign credential cache socket",
-	listenStream: "%C/sigstore/gitsign/cache.sock",
-	directoryMode: "0700",
-	wantedBy: "default.target",
-}).install();
+new SystemdUnit("gitsign-credential-cache.service", {
+  description: "GitSign credential cache",
+  type: "simple",
+  scope: "user",
+  execStart: `${gitSignCredentialCachePath}/gitsign-credential-cache`,
+  wantedBy: "default.target",
+})
+  .install()
+  .enable();
--- rawkOS/utils/systemd/mod.ts
+++ rawkOS/utils/systemd/mod.ts
@@ -2,69 +2,69 @@ import { $ } from "bun";
 import { mkdirSync, writeFileSync } from "node:fs";
 
 interface UnitConfig {
-	type:
-		| "simple"
-		| "oneshot"
-		| "dbus"
-		| "notify"
-		| "forking"
-		| "idle"
-		| "socket";
-	description: string;
-	wantedBy: string;
-	scope: "system" | "user";
+  type:
+    | "simple"
+    | "oneshot"
+    | "dbus"
+    | "notify"
+    | "forking"
+    | "idle"
+    | "socket";
+  description: string;
+  wantedBy: string;
+  scope: "system" | "user";
 }
 
 interface Service extends UnitConfig {
-	execStart: string;
+  execStart: string;
 }
 
 interface Socket extends UnitConfig {
-	type: "socket";
-	listenStream: string;
-	directoryMode: string;
+  type: "socket";
+  listenStream: string;
+  directoryMode: string;
 }
 
 export class SystemdUnit<T extends UnitConfig> {
-	private readonly home: string | undefined;
+  private readonly home: string | undefined;
 
-	constructor(
-		public name: string,
-		public unit: T,
-	) {
-		this.home = import.meta.env.HOME;
-	}
+  constructor(
+    public name: string,
+    public unit: T,
+  ) {
+    this.home = import.meta.env.HOME;
+  }
 
-	private getDirectory() {
-		mkdirSync(`${this.home}/.config/systemd/user`, {
-			recursive: true,
-		});
+  private getDirectory() {
+    mkdirSync(`${this.home}/.config/systemd/user`, {
+      recursive: true,
+    });
 
-		return this.unit.scope === "system"
-			? "/etc/systemd/system"
-			: `${this.home}/.config/systemd/user`;
-	}
+    return this.unit.scope === "system"
+      ? "/etc/systemd/system"
+      : `${this.home}/.config/systemd/user`;
+  }
 
-	install(): this {
-		const unitPath = `${this.getDirectory()}/${this.name}`;
-		writeFileSync(unitPath, new TextEncoder().encode(this.toString()));
-		return this;
-	}
+  install(): this {
+    const unitPath = `${this.getDirectory()}/${this.name}`;
+    writeFileSync(unitPath, new TextEncoder().encode(this.toString()));
+    return this;
+  }
 
-	async enable() {
-		return await $`systemctl enable ${this.name}`;
-	}
+  async enable() {
+    return await $`systemctl enable ${this.unit.scope == "user" ? "--user" : ""} --now ${this.name}`;
+  }
 
-	// Yes, this sucks. I'll refactor when it's working.
-	private toString() {
-		const unit = `[Unit]\nDescription=${this.unit.description}\n\n`;
-		const install = `[Install]\nWantedBy=${this.unit.wantedBy}\n\n`;
+  // Yes, this sucks. I'll refactor when it's working.
+  private toString() {
+    const unit = `[Unit]\nDescription=${this.unit.description}\n\n`;
+    const install = `[Install]\nWantedBy=${this.unit.wantedBy}\n\n`;
 
-		if ("execStart" in this.unit) {
-			return `${unit}[Service]\nType=${this.unit.type}\nExecStart=${this.unit.execStart}\n\n${install}`;
-		}
-		if ("listenStream" in this.unit && "directoryMode" in this.unit) {
-			return `${unit}[Socket]\nType=${this.unit.type}\nListenStream=${this.unit.listenStream}\nDirectoryMode=${this.unit.directoryMode}\n\n${install}`;
-		}
-	}
+    if ("execStart" in this.unit) {
+      return `${unit}[Service]\nType=${this.unit.type}\nExecStart=${this.unit.execStart}\n\n${install}`;
+    }
+    if ("listenStream" in this.unit && "directoryMode" in this.unit) {
+      return `${unit}[Socket]\nType=${this.unit.type}\nListenStream=${this.unit.listenStream}\nDirectoryMode=${this.unit.directoryMode}\n\n${install}`;
+    }
+  }
 }
