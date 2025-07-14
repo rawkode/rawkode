3903138a3b300a9788ad5c63063e8e438bad45d1
David Flanagan <david@rawkode.dev>
2025-07-14 17:29:51 +0100

    fix: vscode fixes

--- nix/modules/home/desktop/visual-studio-code/default.nix
+++ nix/modules/home/desktop/visual-studio-code/default.nix
@@ -5,5 +5,14 @@
     package = pkgs.vscode-fhs;
   };
 
+  home.file.".vscode/argv.json".text = builtins.toJSON {
+    password-store = "gnome-libsecret";
+    ozone-platform-hint = "auto";
+    ozone-platform = "wayland";
+    gtk-version = 4;
+    enable-wayland-ime = true;
+    enable-crash-reporter = false;
+  };
+
   catppuccin.vscode.profiles.default.enable = false;
 }
