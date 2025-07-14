15bcff91a9765e76f64de3f391576586a98c6108
David Flanagan <david@rawkode.dev>
2025-07-14 13:53:13 +0100

    chore: remove codex, tweak starship

--- nix/modules/home/command-line/starship/default.nix
+++ nix/modules/home/command-line/starship/default.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   programs.starship = {
     enable = true;
@@ -6,6 +7,56 @@
 
     settings = {
       add_newline = true;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_state"
+        "$git_status"
+        "$cmd_duration"
+        "$line_break"
+        "$python"
+        "$character"
+      ];
+
+      directory = lib.mkForce { style = "blue"; };
+
+      character = lib.mkForce {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
+      };
+
+      git_branch = lib.mkForce {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+
+      git_status = lib.mkForce {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        # Note the chars in the quotes are zero width spaces for marking word breaks.
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+
+      git_state = lib.mkForce {
+        format = ''\([$state( $progress_current/$progress_total)]($style)\) '';
+        style = "bright-black";
+      };
+
+      cmd_duration = lib.mkForce {
+        format = "[$duration]($style) ";
+        style = "yellow";
+      };
     };
   };
 }
