216c2505b752d675ddb2a2ec8ac5971b4b3576af
David Flanagan <david@rawkode.dev>
2025-08-13 10:45:21 +0100

    feat: goose-cli

--- nix/modules/home/ai/default.nix
+++ nix/modules/home/ai/default.nix
@@ -4,6 +4,8 @@
     inputs.nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system}.claude-code
     inputs.nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system}.claude-desktop
     inputs.nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system}.gemini-cli
+    inputs.nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system}.goose-cli
+    inputs.nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system}.qwen-code
   ];
 
   home.file.".claude/CLAUDE.md".source = ./CLAUDE.md;
