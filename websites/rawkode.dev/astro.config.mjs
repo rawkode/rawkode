17d1d4e558cf36a83897281aba831c6ea3208d0f
David Flanagan <david@rawkode.dev>
2026-05-11 23:48:57 +0100

    feat(rawkode.dev): wire Alteran Cloudflare deployment

--- websites/rawkode.dev/astro.config.mjs
+++ websites/rawkode.dev/astro.config.mjs
@@ -1,5 +1,14 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import alteran from '@alteran/astro';
 
 export default defineConfig({
   site: 'https://rawkode.dev',
+  adapter: cloudflare({ mode: 'advanced', imageService: 'cloudflare' }),
+  integrations: [
+    alteran({
+      debugRoutes: false,
+      includeRootEndpoint: false,
+    }),
+  ],
 });
